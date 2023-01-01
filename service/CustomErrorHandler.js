07e94132139522c8e3ab52149a6f22c97d6d5c9c
Bhavesh <bhaveshpatil856@gmail.com>
2023-01-01 18:02:52 +0530

    login_signup commit

--- service/CustomErrorHandler.js
+++ service/CustomErrorHandler.js
@@ -1,28 +1,26 @@
-class CustomErrorHandler extends Error{
-    constructor(status,msg){
+class CustomErrorHandler extends Error {
+    constructor(status, msg) {
         super();
-        this.status=status,
-        this.message=msg
+        (this.status = status),
+        (this.message = msg);
+    }
 
+    static alreadyExist(message) {
+        console.log("message", message);
+        return new CustomErrorHandler(409, message);
     }
-    static alreadyExist(message)
-    {
-        console.log("message",message)
-        return new CustomErrorHandler(409,message)
+    static wronCredentails(message = "Username or Passowrd is Wrong.!") {
+        return new CustomErrorHandler(401, message);
     }
-    static wronCredentails(message="username or passowrd is wrong")
-    {
-        return new CustomErrorHandler(401,message)
+    static datanotFound(message = "Data Not Found.") {
+        return new CustomErrorHandler(410, message);
     }
-    static datanotFound(message="Datanot found"){
-        return new CustomErrorHandler(410,message);
+    static serverError(message = "Server Error") {
+        return new CustomErrorHandler(405, message);
     }
-    static serverError(message="server error"){
-        return new CustomErrorHandler(405,message)
-    }
-    static serverError(message = 'Internal server error') {
+    static serverError(message = "Internal Server Error") {
         return new CustomErrorHandler(500, message);
     }
-   
+
 }
 export default CustomErrorHandler;
