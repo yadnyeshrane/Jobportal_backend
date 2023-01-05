6d0cda8fc89b0477bedaf5c32543effa330dd8c5
Yadnyesh <yadnyeshrane1998@gmail.com>
2023-01-05 21:25:03 +0530

    job create api chnages

--- models/jobmodel.js
+++ models/jobmodel.js
@@ -14,6 +14,18 @@ const userschema=new Schema({
             requiredSkills:{type:String},
             education:{type:String},
             jobnature:{type:String},
+            comapny_mob:{type:String},
+            comapny_aletrmob:{type:String},
+            adressline_1:{type:String},
+            adressline_2:{type:String},
+            state:{type:String},
+            country:{type:String},
+            postcode:{type:String},
+            position:{type:String},
+            min_exp:{type:Number},
+            max_exp:{type:Number},
+            min_salary:{type:Number},
+            max_salary:{type:Number},
     
 },{timestamps:true})
 export default mongoose.model("Job",userschema,'JobDB')
