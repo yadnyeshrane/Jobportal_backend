e8ff99838573057c0e31f459bed0d5024cb297cf
Yadnyesh <yadnyeshrane1998@gmail.com>
2023-01-06 23:55:44 +0530

    Update jobdetails.js

--- controller/jobdetails.js
+++ controller/jobdetails.js
@@ -7,16 +7,27 @@ const jobdetailscontroller = {
             categorgy: Joi.required(),
             category_enum: Joi.string().required(),
             compnayname: Joi.string().required(),
-            compnaylocation: Joi.string().required(),
+            compnaylocation: Joi.string(),
             companywebiste: Joi.string(),
             companyemail: Joi.string(),
             vacany: Joi.string(),
             jobnature: Joi.string(),
-            salary: Joi.string().required(),
             jobdescription: Joi.string().required(),
             requiredSkills: Joi.string().required(),
             education: Joi.string().required(),
             jobnature: Joi.string().required(),
+            comapny_mob:Joi.string(),
+            comapny_aletrmob:Joi.string(),
+            adressline_1:Joi.string(),
+            adressline_2:Joi.string(),
+            state:Joi.string(),
+            country:Joi.string(),
+            postcode:Joi.string(),
+            position:Joi.string(),
+            min_exp:Joi.string(),
+            max_exp:Joi.string(),
+            min_salary:Joi.string(),
+            max_salary:Joi.string()
         });
         const { error } = registerSchema.validate(req.body);
         if (error) {
@@ -25,6 +36,8 @@ const jobdetailscontroller = {
         const job = new Job({
             categorgy: req.body.categorgy,
             category_enum: Number(req.body.category_enum),
+            comapny_mob:req.body.comapny_mob,
+            comapny_aletrmob:req.body.comapny_aletrmob,
             compnayname: req.body.compnayname,
             compnaylocation: req.body.compnaylocation,
             companywebiste: req.body.companywebiste,
@@ -35,6 +48,16 @@ const jobdetailscontroller = {
             jobdescription: req.body.jobdescription,
             requiredSkills: req.body.requiredSkills,
             education: req.body.education,
+            adressline_1:req.body.adressline_1,
+            adressline_2:req.body.adressline_2,
+            state:req.body.state,
+            country:req.body.country,
+            postcode:req.body.postcode,
+            position:req.body.position,
+            min_exp:Number(req.body.min_exp),
+            max_exp:Number(req.body.max_exp),
+            min_salary:Number(req.body.min_salary),
+            max_salary:Number(req.body.max_salary)
         });
         try {
             const result = await job.save();
